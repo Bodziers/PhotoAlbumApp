b0c3608014ad63932b8032c7b24273ffaa4e744b
Bogdan Pyrc <pyrc.bogdan@gmail.com>
2022-06-08 22:44:16 +0200

    initial commit

---
+++ src/main/java/pl/finmatik/photoalbumapp/PhotoAlbumAppApplication.java
@@ -0,0 +1,13 @@
+package pl.finmatik.photoalbumapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAlbumAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAlbumAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/finmatik/photoalbumapp/PhotoAlbumAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.finmatik.photoalbumapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAlbumAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
